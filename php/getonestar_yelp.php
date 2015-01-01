aac80a617e5eca8f06f6e50967d24eeca558a126
Worathiti Manosroi <w.manosroi@gmail.com>
2015-01-02 00:11:54 +0100

    remove comment

--- php/getonestar_yelp.php
+++ php/getonestar_yelp.php
@@ -1,23 +1,5 @@
 <?php
 
-/**
- * Yelp API v2.0 code sample.
- *
- * This program demonstrates the capability of the Yelp API version 2.0
- * by using the Search API to query for businesses by a search term and location,
- * and the Business API to query additional information about the top result
- * from the search query.
- * 
- * Please refer to http://www.yelp.com/developers/documentation for the API documentation.
- * 
- * This program requires a PHP OAuth2 library, which is included in this branch and can be
- * found here:
- *      http://oauth.googlecode.com/svn/code/php/
- * 
- * Sample usage of the program:
- * `php sample.php --term="bars" --location="San Francisco, CA"`
- */
-
 // Enter the path that the oauth library is in relation to the php file
 require_once('lib/OAuth.php');
 require_once('lib/yelpkey.php');
